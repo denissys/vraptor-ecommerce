7214595f11adc501263aa15fce88d74f5084fd6e
Denis Santos <denissys@gmail.com>
2013-05-29 09:32:28 -0300

    This commit adds data load initial for test system, included user admin and products

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+-- User Admin
+INSERT INTO user (insert_date, document, email, firstName, lastName, password, role) VALUES ('2013-05-29 08:00:14', '783.782.883-66', 'apu@kwik.com.br', 'Apu', 'Nahasapeemapetilon', '123456', 'ADMIN');
+
+-- Products Example 
+INSERT INTO `dbkwikemart`.`product` (insert_date, description, imageFilename, name, price) VALUES ('2013-05-29 08:41:51', 'Deliciosos Legumes congelados', 'legumes-congelado--1.jpg', 'Legumes congelado', 5.5);
+INSERT INTO `dbkwikemart`.`product` (insert_date, description, imageFilename, name, price) VALUES ('2013-05-29 08:42:52', 'Requeijão cremoso', 'requeijao--2.jpg', 'Requeijão', 3.49);
+INSERT INTO `dbkwikemart`.`product` (insert_date, description, imageFilename, name, price) VALUES ('2013-05-29 08:43:53', 'A Bisnaguinha ORIGINAL', 'bisnaguinha--3.gif', 'Bisnaguinha', 4.76);
+INSERT INTO `dbkwikemart`.`product` (insert_date, description, imageFilename, name, price) VALUES ('2013-05-29 08:44:54', 'Em lata', 'coca-cola--4.jpg', 'Coca-Cola', 1.28);
+INSERT INTO `dbkwikemart`.`product` (insert_date, description, imageFilename, name, price) VALUES ('2013-05-29 08:45:55', 'Bombons sortidos', 'caixa-de-bombom--5.jpg', 'Caixa de Bombom', 9.99);
+INSERT INTO `dbkwikemart`.`product` (insert_date, description, imageFilename, name, price) VALUES ('2013-05-29 08:46:56', 'Ótima para o churrasco', 'picanha--6.jpg', 'Picanha', 28.99);
+INSERT INTO `dbkwikemart`.`product` (insert_date, description, imageFilename, name, price) VALUES ('2013-05-29 08:47:57', 'Ótimo para a sua saúde', 'leite-molico--7.jpg', 'Leite Molico', 2.34);
+INSERT INTO `dbkwikemart`.`product` (insert_date, description, imageFilename, name, price) VALUES ('2013-05-29 08:48:58', 'Azeite', 'azeite--8.jpg', 'Ótimo para temperar a sua salada', 14.00);
